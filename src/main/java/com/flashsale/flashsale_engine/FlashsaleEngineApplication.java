61964e2357922a16bfcfcafc97b206b8346c5ebc
HaitaanshDixit <dixithaitaansh@gmail.com>
2026-07-01 19:56:56 +0530

    Initial project setup with Spring Boot skeleton

---
+++ src/main/java/com/flashsale/flashsale_engine/FlashsaleEngineApplication.java
@@ -0,0 +1,13 @@
+package com.flashsale.flashsale_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashsaleEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashsaleEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flashsale/flashsale_engine/FlashsaleEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashsale.flashsale_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashsaleEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
