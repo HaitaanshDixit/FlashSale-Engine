432815f3c0bfddc46038a64254749adaecef0735
HaitaanshDixit <dixithaitaansh@gmail.com>
2026-07-17 16:06:34 +0530

    had to mention the timezone explicitly due to jvm issue

--- src/main/java/com/flashsale/flashsale_engine/FlashsaleEngineApplication.java
+++ src/main/java/com/flashsale/flashsale_engine/FlashsaleEngineApplication.java
@@ -3,9 +3,12 @@ package com.flashsale.flashsale_engine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class FlashsaleEngineApplication {
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(FlashsaleEngineApplication.class, args);
 		System.out.println("Server has started!");
 	}
