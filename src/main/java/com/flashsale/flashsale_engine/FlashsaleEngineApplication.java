5f4e601cc62bcbdbd380f4a4c05ed4569510bf9a
HaitaanshDixit <dixithaitaansh@gmail.com>
2026-07-25 21:11:08 +0530

    @EnableCaching annotation implemented to allow redis caching

--- src/main/java/com/flashsale/flashsale_engine/FlashsaleEngineApplication.java
+++ src/main/java/com/flashsale/flashsale_engine/FlashsaleEngineApplication.java
@@ -2,10 +2,12 @@ package com.flashsale.flashsale_engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableCaching
 public class FlashsaleEngineApplication {
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
