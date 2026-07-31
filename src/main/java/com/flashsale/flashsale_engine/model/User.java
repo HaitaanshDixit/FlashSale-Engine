e9ef57e5b8fb5b62b67eac0b16b762c412b97e6a
HaitaanshDixit <dixithaitaansh@gmail.com>
2026-07-31 21:43:56 +0530

    added the User entity

---
+++ src/main/java/com/flashsale/flashsale_engine/model/User.java
@@ -0,0 +1,28 @@
+package com.flashsale.flashsale_engine.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    // This stores the BCrypt-scrambled password, never the raw one.
+    @Column(nullable = false)
+    private String password;
+}
